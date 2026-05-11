93f2263a731f92dbc40399fbc349cc76d546fee5
Gabe Cook <git@gabe565.com>
2026-05-11 18:50:04 -0500

    chore(cobrax): fallback to versionx if annotation is unset

--- cobrax/version.go
+++ cobrax/version.go
@@ -1,18 +1,30 @@
 package cobrax
 
 import (
+	"runtime/debug"
+
 	"gabe565.com/utils/httpx"
+	"gabe565.com/utils/versionx"
 	"github.com/spf13/cobra"
 )
 
 // GetVersion gets the raw version set by WithVersion.
 func GetVersion(cmd *cobra.Command) string {
-	return cmd.Root().Annotations[VersionKey]
+	if v, ok := cmd.Root().Annotations[VersionKey]; ok {
+		return v
+	}
+	if info, ok := debug.ReadBuildInfo(); ok {
+		return info.Main.Version
+	}
+	return ""
 }
 
 // GetCommit gets the commit set by WithVersion.
 func GetCommit(cmd *cobra.Command) string {
-	return cmd.Root().Annotations[CommitKey]
+	if v, ok := cmd.Root().Annotations[CommitKey]; ok {
+		return v
+	}
+	return versionx.CommitFromVCS().Short()
 }
 
 // BuildUserAgent generates a value to be used with httpx.UserAgentTransport.
